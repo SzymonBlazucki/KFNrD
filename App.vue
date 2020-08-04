d43fdaeb8feb584b1d8a3a68f62a2bc45deca5a1
SzymonBlazucki <68511485+SzymonBlazucki@users.noreply.github.com>
2020-08-05 00:37:58 +0200

    added matrixCreator

---
+++ App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    {{title}}<br/>
+    description - to be done later
+    <!--<img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <h2> Filtration</h2>
+  <mat :initial="[1/3, 1/3, 1/3]" :matr='matrix1'> </mat>
+    <h2> Fluorescence</h2>
+  <mat :initial="[1/3, 1/3, 1/3]" :matr='matrix2'> </mat>
+    <h2> Another type of the fluorescence</h2>
+  <mat :initial="[1/3, 1/3, 1/3]" :matr='matrix3'> </mat>
+  And now you can create your own operation. Just set up appropriate values in the input table and see what happens.
+  <matrixCreator/> 
+
+  <ball/>
+  </div>
+
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+import { Dimension, Vector, Cx, Gates, Operator } from "quantum-tensors";
+import { KetViewer, MatrixViewer } from "bra-ket-vue";
+import mat from './components/mat.vue';
+import matrixCreator from './components/matrixCreator.vue';
+
+const colorDim = new Dimension('color', 3, ['R','G','B']) 
+//mat: () => import('../components/mat.vue')
+@Component({
+  components: {
+    HelloWorld,
+    KetViewer,
+    MatrixViewer,
+    mat,
+    matrixCreator,
+  },
+  data: function() {
+    return {
+      title: 'Project Matrix',
+      matrix1: [
+  ['R', 'R', Cx(1)],
+  ['G', 'G', Cx(1)],
+],
+      matrix2: [
+  ['R', 'R', Cx(1)],
+  ['R', 'G', Cx(1)],
+  ['R', 'B', Cx(1)]
+],
+       matrix3: [
+  ['G', 'G', Cx(1)],
+  ['G', 'B', Cx(1)],
+],
+      value: null
+    };
+  },
+   
+  
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ components/mat.vue
@@ -0,0 +1,67 @@
+<template>
+<div>
+  <br/>
+  <h5> Initial Conditions </h5>
+    <KetViewer :vector="start" :dark-mode="false"/> <br/>
+  <h5> Operation </h5>
+    <MatrixViewer :operator-raw="vis" :dark-mode="false"/> <br/>
+  <h5> product vector </h5>
+    <KetViewer :vector="end" :dark-mode="false"/> <br/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './HelloWorld.vue';
+import { Dimension, Vector, Cx, Gates, Operator } from "quantum-tensors";
+import { KetViewer, MatrixViewer } from "bra-ket-vue";
+const colorDim = new Dimension('color', 3, ['R','G','B']) 
+
+function check (letter){
+  if (letter == 'R') return 0;
+  else if (letter == 'G') return 1;
+  else if (letter == 'B') return 2;
+}
+
+function operation (givenVector, matrix){
+  const emptyVector = [["R", Cx(0)], ["G", Cx(0)],['B', Cx(0)]]
+  for (let i=0; i<matrix.length; i++){
+  emptyVector[check(matrix[i][0])][1].re += givenVector[check(matrix[i][0])][1].re*matrix[i][2].re
+  }
+  return emptyVector
+}
+
+function vectorCreator (arr){
+  return [["R", Cx(arr[0])], ["G", Cx(arr[1])],['B', Cx(arr[2])]] 
+}
+
+export default  {
+
+  components: {
+    KetViewer,
+    MatrixViewer
+  },
+  props: {
+    initial: Array,
+    matr: Array
+  } ,
+      
+
+  data: function() {
+      return{
+      person: this.initial[2],
+      mata: this.matr,
+      start: Vector.fromSparseCoordNames(
+        vectorCreator(this.initial),
+        [colorDim]),
+      vis: Operator.fromSparseCoordNames(
+        this.matr, [colorDim]),
+       end:  Vector.fromSparseCoordNames(
+        operation( vectorCreator(this.initial), this.matr),
+        [colorDim]),
+  }},
+
+
+}
+
+</script>
